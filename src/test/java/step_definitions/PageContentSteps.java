dda18ba73c3d4a0b5e1d1e904e115ec9855afaa5
jorgeejgonzalez <jorgeejgonzalez@gmail.com>
2015-12-29 18:19:00 -0300

    added additional steps to the PageContent feature

--- src/test/java/step_definitions/PageContentSteps.java
+++ src/test/java/step_definitions/PageContentSteps.java
@@ -3,6 +3,7 @@ package step_definitions;
 import api.BrandPageData;
 import api.SeleniumAction;
 import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -41,10 +42,24 @@ public class PageContentSteps {
         assertThat(title).isEqualTo(testData.getTitle());
     }
 
+    @Then("^the title should be  under (\\d+) characters$")
+    public void the_title_should_be_under_characters(int max) throws Throwable {
+        String title = brandPage.getBrandData().getTitle();
+        assertThat(title.length()).isLessThanOrEqualTo(max);
+    }
+
+
     @Then("^the canonical url should match the TestData$")
     public void the_canonical_url_should_match_the_TestData() throws Throwable {
         String canonical = brandPage.getBrandData().getCanonical();
         assertThat(canonical).isEqualTo(testData.getCanonical());
+    }
+
+    @And("^the meta description should not be empty$")
+    public void theMetaDescriptionShouldNotBeEmpty() throws Throwable {
+        assertThat(brandPage.hasMetaDescription()).isTrue();
+        String metaDescription = brandPage.getBrandData().getMetaDescription();
+        assertThat(metaDescription).isNotEmpty();
     }
 
     @Then("^the meta description should match the TestData$")
@@ -53,10 +68,32 @@ public class PageContentSteps {
         assertThat(metaDescription).isEqualTo(testData.getMetaDescription());
     }
 
+    @Then("^the meta description should be between (\\d+) and (\\d+) characters$")
+    public void the_meta_description_should_be_between_and_characters(int min, int max) throws Throwable {
+        String metaDescription = brandPage.getBrandData().getMetaDescription();
+        assertThat(metaDescription.length())
+                .isGreaterThanOrEqualTo(min)
+                .isLessThanOrEqualTo(max);
+    }
+
+    @And("^the header one should not be empty$")
+    public void theHeaderOneShouldNotBeEmpty() throws Throwable {
+        assertThat(brandPage.hasHeader1()).isTrue();
+        String header1 = brandPage.getBrandData().getHeader1();
+        assertThat(header1).isNotEmpty();
+    }
+
     @Then("^the header one should match the TestData$")
     public void the_header_one_should_match_the_TestData() throws Throwable {
         String header1 = brandPage.getBrandData().getHeader1();
         assertThat(header1).isEqualTo(testData.getHeader1());
+    }
+
+    @And("^the description should not be empty$")
+    public void theDescriptionShouldNotBeEmpty() throws Throwable {
+        assertThat(brandPage.hasDescription()).isTrue();
+        String description = brandPage.getBrandData().getDescription();
+        assertThat(description).isNotEmpty();
     }
 
     @Then("^the description should match the TestData$")
@@ -65,8 +102,13 @@ public class PageContentSteps {
         assertThat(description).isEqualTo(testData.getDescription());
     }
 
-    @Then("^the breadcrumbs should match the TestData$")
-    public void the_breadcrumbs_should_match_the_TestData() throws Throwable {
+    @And("^the breadcrumbs should not be empty$")
+    public void theBreadcrumbsShouldNotBeEmpty() throws Throwable {
+        assertThat(brandPage.hasBreadcrumbs()).isTrue();
+    }
+
+    @Then("^the breadcrumbs text should match the TestData$")
+    public void the_breadcrumbs_text_should_match_the_TestData() throws Throwable {
         String breadcrumbs = brandPage.getBrandData().getBreadcrumbsText();
         assertThat(breadcrumbs).isEqualTo(testData.getBreadcrumbsText());
     }
@@ -76,5 +118,6 @@ public class PageContentSteps {
         List<String> breadcrumbs = brandPage.getBrandData().getBreadcrumbs();
         assertThat(breadcrumbs).containsAll(testData.getBreadcrumbs());
     }
+
 
 }
--- src/test/resources/features/PageContent.feature
+++ src/test/resources/features/PageContent.feature
@@ -6,11 +6,17 @@ Feature: The Brand Page content must follow a set of rules
     Given I select the url "<URL>" from the TestData
     When I navigate to the brand page
     Then the title should match the TestData
+    And the title should be  under 70 characters
     And the canonical url should match the TestData
+    And the meta description should not be empty
     And the meta description should match the TestData
+    And the meta description should be between 145 and 165 characters
+    And the header one should not be empty
     And the header one should match the TestData
+    And the description should not be empty
     And the description should match the TestData
-    And the breadcrumbs should match the TestData
+    And the breadcrumbs should not be empty
+    And the breadcrumbs text should match the TestData
     And the breadcrumbs links should match the TestData
 
     Examples:
