2d89c26b9d8f2af9f2647f8713bc315daea9e86e
jorgeejgonzalez <jorgeejgonzalez@gmail.com>
2016-01-16 13:21:43 -0300

    fixed properties address

--- jenkins-localhtmlunit.sh
+++ jenkins-localhtmlunit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -f src/test/resources/features-processed/*.feature
-cp -f src/test/resources/properties/TestConfig-LocalFirefox.properties src/test/resources/properties/TestConfig.properties
+cp -f src/test/resources/properties/TestConfig-LocalHtmlUnit.properties src/test/resources/properties/TestConfig.properties
 mvn clean test
