77d9c2600266561cf24b03a6d38121f6c5101242
jorgeejgonzalez <jorgeejgonzalez@gmail.com>
2016-01-16 13:36:19 -0300

    fixed the jenkins batch files for succesful copy

--- jenkins-localchrome.bat
+++ jenkins-localchrome.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 del /F src\test\resources\features-processed\*.feature
-xcopy /s src\test\resources\properties\TestConfig-LocalChrome.properties src\test\resources\properties\TestConfig.properties
+copy /Y src\test\resources\properties\TestConfig-LocalChrome.properties src\test\resources\properties\TestConfig.properties
 mvn clean test
--- jenkins-localfirefox.bat
+++ jenkins-localfirefox.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 del /F src\test\resources\features-processed\*.feature
-xcopy /s src\test\resources\properties\TestConfig-LocalFirefox.properties src\test\resources\properties\TestConfig.properties
+copy /Y src\test\resources\properties\TestConfig-LocalFirefox.properties src\test\resources\properties\TestConfig.properties
 mvn clean test
