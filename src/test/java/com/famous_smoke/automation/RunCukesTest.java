30a044b95df180537bbcd293302ec1e78e566376
jorgeejgonzalez <jorgeejgonzalez@gmail.com>
2016-01-31 15:53:29 -0300

    added support to cucumber reports jenkins plugin

--- src/test/java/com/famous_smoke/automation/RunCukesTest.java
+++ src/test/java/com/famous_smoke/automation/RunCukesTest.java
@@ -7,11 +7,12 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+		strict = false,
 		features = {
 				"classpath:features/setup",
 				"classpath:features-processed"
 		},
-		plugin = {"pretty", "html:target/cucumber-html-report","json:cucumber.json"},
+		plugin = {"pretty", "html:target/site/cucumber-pretty","json:target/cucumber.json"},
 		tags = {}
 		)
 public class RunCukesTest {
