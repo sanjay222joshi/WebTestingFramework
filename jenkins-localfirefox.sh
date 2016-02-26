54545ec58261c985c8f70c04b6d2508740513067
jorgeejgonzalez <jorgeejgonzalez@gmail.com>
2016-02-26 00:14:52 -0300

    added the tags filter to the maven run in the jenkins scripts

--- jenkins-localfirefox.sh
+++ jenkins-localfirefox.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -f src/test/resources/features/processed/*.feature
 cp -f src/test/resources/properties/TestConfig-LocalFirefox.properties src/test/resources/properties/TestConfig.properties
-mvn clean test
+mvn clean test -Dcucumber.options="--tags '@brands'"
